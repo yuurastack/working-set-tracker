9cd06bc2892d1d63089840dff393394a43c1dae3
yuurastack <felipeaperezs@gmail.com>
2022-05-22 13:39:43 -0400

    added % increase from last set

--- src/app/components/workout-card/workout-card.component.ts
+++ src/app/components/workout-card/workout-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Excercise, Set } from 'src/app/models/exercices';
 import { EventEmitter } from '@angular/core';
@@ -8,7 +8,7 @@ import { EventEmitter } from '@angular/core';
   templateUrl: './workout-card.component.html',
   styleUrls: ['./workout-card.component.scss']
 })
-export class WorkoutCardComponent implements OnInit {
+export class WorkoutCardComponent {
   @Input() public exercise: Excercise;
   @Output() public setAdded = new EventEmitter();
   @Output() public cardDeleted = new EventEmitter();
@@ -18,29 +18,39 @@ export class WorkoutCardComponent implements OnInit {
     setReps : new FormControl('', [Validators.pattern(this.setPattern)])
   });
 
+  
+  get lastSet() {
+    return this.exercise.sets && this.exercise.sets[1] ? new Date(this.exercise.sets[0].time).toDateString()
+    + ':\xa0\xa0' + this.exercise.sets[1].reps  + ' X ' + this.exercise.sets[1].weight : 'Add another set to see progress'
+  }
+  
+  get firstSet() {
+    return this.exercise.sets && this.exercise.sets[0].reps ? new Date(this.exercise.sets[0].time).toDateString()
+    + ':\xa0\xa0' + this.exercise.sets[0].reps + ' X ' + this.exercise.sets[0].weight : 'Add your first set (ex: reps x weight)'
+  }
+
+  get setDifference() {
+    if (!this.exercise.sets) return '0 % up';
+    const diff = (this.getOneRepMaxEpley(this.exercise.sets[0]) / this.getOneRepMaxEpley(this.exercise.sets[1]) -1)* 100;
+    const diffStr = diff.toFixed(2);
+    return  diff > 0 ? diffStr + '% up' : diffStr+ '% down'
+  }
+  
+  
+  
+  
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+  
   getErrorMessage() {
     return this.setsForm.controls['setReps'].hasError('pattern') ? 'Not a valid set, try number x number' : '';
   }
-
-  get lastSet() {
-    return this.exercise.sets && this.exercise.sets[1] ? this.exercise.sets[1].reps  + ' X ' + this.exercise.sets[1].weight : 'Train more!!!!'
-  }
-
-  get firstSet() {
-    return this.exercise.sets && this.exercise.sets[0].reps ? this.exercise.sets[0].reps + ' X ' + this.exercise.sets[0].weight : 'Add your first set (ex: 1x1)'
-  }
-
-
   
-  constructor(private formBuilder: FormBuilder) {
-
+  public getOneRepMaxEpley(set: Set ): number {
+    return set? parseFloat((set.weight * (1 + set.reps/30)).toFixed(1)) : 0;
   }
-
-
-  ngOnInit(): void {
-
-  }
-
+  
   onSubmit(setReps: string): void {
     let [repsStr, weightStr ] = setReps.split('x');
     const reps = parseInt(repsStr);
